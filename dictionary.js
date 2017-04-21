067ff34105f406ebf3bd6923f6ffeef1ee179f40
Alexandru Mereacre <mereacre@gmail.com>
2017-04-21 15:02:21 +0100

    Added dictionary

--- dictionary.js
+++ dictionary.js
@@ -19,14 +19,16 @@ fs.open(process.argv[3], "w", (err, fd) => {
                 process.exit(1);
             } else {
                 const dictionary = {};
-                csv()
+                csv({delimiter: ","})
                     .fromFile(process.argv[2])
                     .on('json', (entryCsv) => {
-                        dictionary[entryCsv[process.argv[4]]] = 1;
+                        console.log(entryCsv);
+                        const entry = _.omit(entryCsv, [process.argv[4]])
+                        dictionary[entryCsv[process.argv[4]]] = entry;
                     })
                     .on("done", (error) => {
                         _.map(dictionary, (value, key) => {
-                            fs.writeFileSync(process.argv[3], "\"" + key+ "\",\n", {flag: "a"}, (err) => {
+                            fs.writeFileSync(process.argv[3], "\"" + key+ "\": "+JSON.stringify(value)+",\n", {flag: "a"}, (err) => {
                                 if(err) {
                                     console.log(err);
                                     process.exit(1);
